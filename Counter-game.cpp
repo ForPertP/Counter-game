cec65597dd21f7118251178fd047bda8a28af171
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2022-06-24 20:37:25 +0900

    Update Counter-game.cpp

--- Counter-game.cpp
+++ Counter-game.cpp
@@ -1,1 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'counterGame' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts LONG_INTEGER n as parameter.
+ */
+
+string counterGame(long n)
+{
+    std::string result{"Richard"};
+    long count = 0 ;
+    n = n - 1;
+    
+    return result;
+}
 
