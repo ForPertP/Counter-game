f48dacf7adf7f597ff453618d30d6b03cb9c2c83
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2026-06-14 01:17:11 +0900

    Update Counter-game.cs

--- Counter-game.cs
+++ Counter-game.cs
@@ -1,1 +1,33 @@
-Counter-game
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+using System.Numerics;
+
+class Result
+{
+
+    /*
+     * Complete the 'counterGame' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts LONG_INTEGER n as parameter.
+     */
+
+    public static string counterGame(long n)
+    {
+        int turns = BitOperations.PopCount((ulong)(n - 1));
+        
+        return (turns & 1) == 1 ? "Louise" : "Richard";
+    }
+
+}
