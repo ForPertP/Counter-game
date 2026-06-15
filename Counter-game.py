ba3a7ad0ab736c1ef12b880db44f1c0883d0a253
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2026-06-16 00:22:28 +0900

    Update Counter-game.py

--- Counter-game.py
+++ Counter-game.py
@@ -13,6 +13,9 @@ import sys
 # The function accepts LONG_INTEGER n as parameter.
 #
 
+def counterGame(n):
+    turns = (n - 1).bit_count()
+    return "Louise" if (turns & 1) == 1 else "Richard"
     
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
